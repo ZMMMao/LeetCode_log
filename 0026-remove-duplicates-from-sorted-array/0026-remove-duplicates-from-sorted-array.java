225873e1c817ffddb7978aa81d8ccf2878b47223
Corey <150191535+ZMMMao@users.noreply.github.com>
2025-05-05 17:44:53 -0400

    Time: 1 ms (63.22%), Space: 45 MB (40.42%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -20,13 +20,13 @@ class Solution {
         int res = 0;
         int n = nums.length;
         if(n == 0) return res;
-        int left = 0;
-        for(int right = 0; right < n; right++){
+        int left = 1;
+        for(int right = 1; right < n; right++){
             if(right > 0 && nums[right] == nums[right - 1]) continue;
             nums[left] = nums[right];
             left++;
-            res++;
+            
         }
-        return res;
+        return left;
     }
 }
