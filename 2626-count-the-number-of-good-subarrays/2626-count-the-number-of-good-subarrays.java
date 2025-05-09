9c66e11e1d98da43c9dddd5f68c50cc549d24dfa
Corey <150191535+ZMMMao@users.noreply.github.com>
2025-05-09 17:51:52 -0400

    Time: 55 ms (66.03%), Space: 60.8 MB (42.13%) - LeetHub

---
+++ 2626-count-the-number-of-good-subarrays/2626-count-the-number-of-good-subarrays.java
@@ -0,0 +1,27 @@
+/**
+Algo: linear sorting
+Data Structure: HashMap - counting for pairs
+TC: O(n)
+SC: O(n)
+ */
+class Solution {
+    public long countGood(int[] nums, int k) {
+        Map<Integer, Integer> count = new HashMap<>();
+        int n = nums.length;
+        int pair = 0;
+        int left = 0;
+        long res = 0;
+        for(int i = 0; i < n; i++){
+            count.put(nums[i], count.getOrDefault(nums[i], 0) + 1);
+            pair += count.get(nums[i]) - 1;
+            
+            while(pair >= k){
+                res += n - i;
+                pair-= count.get(nums[left]) - 1;
+                count.put(nums[left], count.get(nums[left]) - 1);
+                left++;
+            }        
+        }
+        return res;
+    }
+}
