a1a96a4a3e18cd2d1bffb34e99ba550d8ef7123f
Corey <150191535+ZMMMao@users.noreply.github.com>
2025-05-10 14:51:54 -0400

    Time: 4 ms (99.98%), Space: 47.9 MB (38.18%) - LeetHub

---
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -0,0 +1,23 @@
+/**
+Algo: linear sort - two pointer, sliding window
+Data Structure: array
+TC: O(n)
+SC: O(1)
+ */
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        int res = 0;
+        int n = nums.length;
+        int left = 0;
+        int product = 1;
+        for(int right = 0; right < n; right++){
+            product *= nums[right];
+            while(left <= right && product >= k){
+                product /= nums[left];
+                left++;
+            }
+            res += right - left + 1;
+        }
+        return res;
+    }
+}
