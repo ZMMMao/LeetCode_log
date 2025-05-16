e3b53474ec14b6772f83e64d2f1b2dd994cdc5ea
Corey <150191535+ZMMMao@users.noreply.github.com>
2025-05-16 15:21:26 -0400

    Time: 49 ms (51%), Space: 47.6 MB (65.16%) - LeetHub

---
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.java
@@ -0,0 +1,29 @@
+/**
+Algo: Sliding window
+Data Structure:
+TC:
+SC:
+ */
+class Solution {
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        return helper(nums, k) - helper(nums, k + 1);
+    }
+
+    public int helper(int[] nums, int k){
+        Map<Integer, Integer> count = new HashMap<>();
+        int res = 0;
+        int left = 0;
+        for(int right = 0; right < nums.length; right++){
+            int curr = nums[right];
+            count.put(curr, count.getOrDefault(curr, 0) + 1);
+            while(count.size() >= k){
+                int l = nums[left];
+                count.put(l, count.get(l) - 1);
+                if(count.get(l) == 0) count.remove(l);
+                left++;
+            }
+            res += left;
+        }
+        return res;
+    }
+}
