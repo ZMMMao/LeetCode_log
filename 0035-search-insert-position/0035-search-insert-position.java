55401c4f49bd5fb1a860f61af2688d383a62eb8b
Corey <150191535+ZMMMao@users.noreply.github.com>
2025-05-17 20:33:47 -0400

    Time: 0 ms (100%), Space: 43.1 MB (39.15%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,21 @@
+/**
+Algo: binary search
+Data Stucture: array
+TC: O(n)
+SC: O(1)
+ */
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = -1;
+        int right = nums.length;
+        while(left + 1 < right){
+            int mid = left + (right - left)/2;
+            if(nums[mid] >= target){
+                right = mid;
+            }else{
+                left = mid;
+            }
+        }
+        return right;
+    }
+}
