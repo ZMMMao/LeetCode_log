b2154dfa59d00b7f7c6ebd511921c4a6f66d6fcd
Corey <150191535+ZMMMao@users.noreply.github.com>
2025-05-20 16:20:55 -0400

    Time: 0 ms (100%), Space: 44.8 MB (91.73%) - LeetHub

---
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.java
@@ -0,0 +1,24 @@
+/**
+Algo:
+Data Structure:
+TC:
+SC:
+ */
+class Solution {
+    public int maximumCount(int[] nums) {
+        int neg = binarySearch(nums, 0);
+        int pos = nums.length - binarySearch(nums, 1);
+        return Math.max(neg, pos);
+    }
+
+    public int binarySearch(int[] nums, int target){
+        int left = -1;
+        int right = nums.length;
+        while(left + 1 < right){
+            int mid = left + (right - left) / 2;
+            if(nums[mid] >= target) right = mid;
+            else{left = mid;}
+        }
+        return right;
+    }
+}
