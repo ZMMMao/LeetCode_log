737b3a5f16fd05d84deb0ba3953404f5ec787119
Corey <150191535+ZMMMao@users.noreply.github.com>
2025-05-24 22:29:58 -0400

    Time: 5 ms (99.33%), Space: 46 MB (6.77%) - LeetHub

---
+++ 2469-longest-subsequence-with-limited-sum/2469-longest-subsequence-with-limited-sum.java
@@ -0,0 +1,42 @@
+/**
+Algo: Binary Search
+Data Structure: array
+TC: O(nlogn + logm)
+SC: O(n + m)
+*/
+
+class Solution {
+    public int[] answerQueries(int[] nums, int[] queries) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int m = queries.length;
+        
+        if(n == 0 || m == 0) return new int[m];
+        int[] ans = new int[m];
+        int[] preSum = new int[n];
+        preSum[0] = nums[0];
+        for(int i = 1; i < n; i++){
+            preSum[i] = preSum[i - 1] + nums[i];
+        }
+
+        for(int i = 0; i < m; i++){
+            if(preSum[0] > queries[i]) ans[i] = 0; 
+            else{ans[i] = binarySearch(preSum, queries[i]);}
+        }
+        return ans;
+    }
+
+    public int binarySearch(int[] preSum, int target){
+        int left = 0;
+        int right = preSum.length - 1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(preSum[mid] > target){
+                right = mid - 1;
+            } else{
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
