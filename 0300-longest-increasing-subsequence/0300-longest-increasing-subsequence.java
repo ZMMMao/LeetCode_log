54fde6da622f77626e5447bc37b7738059ff2a67
Corey <150191535+ZMMMao@users.noreply.github.com>
2025-10-03 14:52:55 -0400

    Time: 41 ms (60.89%), Space: 44.3 MB (58.58%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,40 +1,40 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
+    //     int n = nums.length;
+    //     int[] memo = new int[n];
+    //     int max = 0;
+    //     for(int i = 0; i < n; i++){
+    //         max = Math.max(max, dfs(i, memo, nums));
+    //     }
+    //     return max;
+    // }
+
+    // public int dfs(int idx, int[] memo, int[] nums){
+    //     if(memo[idx] > 0) return memo[idx];
+
+    //     int res = 0;
+    //     for(int j = 0; j < idx; j++){
+    //         if(nums[j] < nums[idx]){
+    //             res = Math.max(res, dfs(j, memo, nums));
+    //         }
+    //     }
+    //     res++;
+    //     return memo[idx] = res;
+    // }
         int n = nums.length;
         int[] memo = new int[n];
         int max = 0;
         for(int i = 0; i < n; i++){
-            max = Math.max(max, dfs(i, memo, nums));
+            for(int j = 0; j < i; j++){
+                if(nums[j] < nums[i]){
+                    memo[i] = Math.max(memo[i], memo[j]);
+                }
+            }
+            memo[i]++;
+            max = Math.max(max, memo[i]);
         }
         return max;
     }
-
-    public int dfs(int idx, int[] memo, int[] nums){
-        if(memo[idx] > 0) return memo[idx];
-
-        int res = 0;
-        for(int j = 0; j < idx; j++){
-            if(nums[j] < nums[idx]){
-                res = Math.max(res, dfs(j, memo, nums));
-            }
-        }
-        res++;
-        return memo[idx] = res;
-    }
-        // int n = nums.length;
-        // int[] memo = new int[n];
-        // int max = 0;
-        // for(int i = 0; i < n; i++){
-        //     for(int j = 0; j < i; j++){
-        //         if(nums[j] < nums[i]){
-        //             memo[i] = Math.max(memo[i], memo[j]);
-        //         }
-        //     }
-        //     memo[i]++;
-        //     max = Math.max(max, memo[i]);
-        // }
-        // return max;
-    
 }
 /**
 TC: O(n^2)
