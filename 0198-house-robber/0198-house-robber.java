dd9129c9dd68e28bb97d019c2cd74a99b821f4ed
Corey <150191535+ZMMMao@users.noreply.github.com>
2025-10-02 22:43:16 -0400

    Time: 0 ms (100%), Space: 41 MB (61.69%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,5 +1,28 @@
 class Solution {
     public int rob(int[] nums) {
+        /**
+        DP optimized O(1) space
+         */
+        int n = nums.length;
+        int[] max = new int[n];
+        int f0 = 0;
+        int f1 = 0;
+
+        for(int num : nums){
+            int newMax = Math.max(f1, f0 + num);
+            f0 = f1;
+            f1 = newMax;
+        }
+        return f1;
+    }
+}
+
+/**
+recursion + memo array
+TC: O(n)
+SC: O(n)
+
+    public int rob(int[] nums) {
         int n = nums.length;
         int[] memo = new int[n];
         Arrays.fill(memo, -1);
@@ -13,9 +36,4 @@ class Solution {
         }
         return memo[idx] = Math.max(nums[idx] + dfs(nums, memo, idx - 2), dfs(nums, memo, idx - 1));
     }
-}
-/**
-recursion + memo array
-TC: O(n)
-SC: O(n)
 */ 
