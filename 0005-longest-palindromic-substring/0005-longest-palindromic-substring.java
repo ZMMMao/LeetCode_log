8fb386d425c275ef0e94bf38435cdb2a1253be5d
Corey <150191535+ZMMMao@users.noreply.github.com>
2025-10-12 22:56:06 -0400

    Time: 20 ms (42.68%), Space: 45.6 MB (20.05%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,16 +1,17 @@
 class Solution {
     public String longestPalindrome(String s) {
+// two pointer solution
         String res = "";
         for(int i = 0; i < s.length(); i++){
-            String s1 = palindrom(s, i, i);
-            String s2 = palindrom(s, i, i+1);
+            String s1 = palindrome(s, i, i);
+            String s2 = palindrome(s, i, i+1);
             res = res.length() > s1.length() ? res : s1;
             res = res.length() > s2.length() ? res : s2;
-        }
+        }   
         return res;
     }
 
-    String palindrom(String s, int l, int r){
+    public String palindrome(String s, int l, int r){
         while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)){
             l--;
             r++;
@@ -18,3 +19,7 @@ class Solution {
         return s.substring(l+1, r);
     }
 }
+/**
+TC: O(n^2)
+SC: O(1)
+ */
