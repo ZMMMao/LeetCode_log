a1b5f1e7442edb785b98c771e97eb47e3579bf07
Corey <150191535+ZMMMao@users.noreply.github.com>
2025-11-25 00:21:54 -0500

    Time: 7 ms (24.72%), Space: 48.3 MB (22.85%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.java
+++ 0252-meeting-rooms/0252-meeting-rooms.java
@@ -1,15 +1,27 @@
 /**
-Algo: array-sort and check overlap
-Data Structure: array
-TC: O(nlogn)
-SC: O(n)
-
+input/output:
+    - input: int[][] intervals
+    -input length: [0, 10^4]
+    -input range: [0, 10^5]
+    -output boolean isValid
+edge case: 
+    - 0 return true
+Approach:
+    sweep to check overlaps
+    - sort with start
+    - check prev end and curr start ovelapping
+    TC: O(nlogn)
+    SC: O(1)
  */
 class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        if(intervals.length == 0) return true;
+
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        int curr = intervals[0][1];
         for(int i = 1; i < intervals.length; i++){
-            if(intervals[i - 1][1] > intervals[i][0]) return false;
+            if(intervals[i][0] < curr) return false;
+            curr = intervals[i][1];
         }
         return true;
     }
