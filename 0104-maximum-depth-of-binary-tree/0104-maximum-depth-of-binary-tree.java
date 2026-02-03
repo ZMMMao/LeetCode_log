3a3253b2075a4dcbddfdebc8c0d51a0fc23427dc
Corey <150191535+ZMMMao@users.noreply.github.com>
2026-02-03 15:36:30 -0500

    Time: 0 ms (100%), Space: 44.5 MB (60.87%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -13,16 +13,25 @@
  *     }
  * }
  */
- /**
- if null return 0
- TC: O(n)
- SC: O(n)
-  */
+/**
+input:
+    root
+output:
+    int depth
+edge case:
+    root == null, return 0
+approach:
+    post-order traverse
+    TC: O(n)
+    SC: O(1)
+ */
 class Solution {
     public int maxDepth(TreeNode root) {
         if(root == null) return 0;
+
         int left = maxDepth(root.left);
         int right = maxDepth(root.right);
-        return Math.max(left, right) + 1;
+    
+        return Math.max(left, right) + 1;   
     }
 }
