5ed1133dcb91b3228062c4340e3302fa5e83714b
Corey <150191535+ZMMMao@users.noreply.github.com>
2026-02-17 12:39:51 -0500

    Time: 2 ms (88.42%), Space: 49.2 MB (88.71%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -8,40 +8,56 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+/**
+input:
+    ListNode
+ouput:
+    void, reorder list
+edge case:
+    head == null return
+approach:
+    1. find midpoint
+    2. reverse second half list
+    3. insert second half one-by-one
+    TC: O(n)
+    SC: O(1)
+  */
 class Solution {
     public void reorderList(ListNode head) {
-        if(head == null) return;
-        ListNode slow = head;
-        ListNode fast = head;
+        if(head == null || head.next == null) return;
+
+        ListNode slow = head, fast = head;
+
+        // find midpoint
         while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
         }
+
         ListNode second = slow.next;
         slow.next = null;
+
+        // reverse second half
         ListNode prev = null;
-        ListNode curr = second;
-        while(curr != null){
-            ListNode nxt = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = nxt;
-        }
-        ListNode one = head;
-        second = prev;
         while(second != null){
-            ListNode tmp1 = one.next;
+            ListNode nxt = second.next;
+            second.next = prev;
+            prev = second;
+            second = nxt;
+        }
+        
+        // interleave second half
+        second = prev;
+        ListNode first = head;
+        while(second != null){
+            ListNode tmp1 = first.next;
             ListNode tmp2 = second.next;
-            one.next = second;
+
+            first.next = second;
             second.next = tmp1;
-            one = tmp1;
+
+            first = tmp1;
             second = tmp2;
         }
-        return;
     }
 }
-/**
-mid-point reverse + insertion
-TC: O(n)
-SC: O(1)
- */
