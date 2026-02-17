64d8461165e6243f7333f77daa1aa23f749e5570
Corey <150191535+ZMMMao@users.noreply.github.com>
2026-02-17 12:13:34 -0500

    Time: 0 ms (100%), Space: 47.1 MB (14.19%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -9,19 +9,38 @@
  *     }
  * }
  */
+ /**
+ input:
+    type: ListNode
+    range: [0, 10^4]
+    node.val: [-10^5, 10^5]
+output:
+    boolean cycle
+edge case:
+    head == null return false;
+approach:
+    fast and slow pointer
+    if fast == null no cycle
+    if fast meet slow, contains cycle
+    TC: O(n)
+    SC: O(1)
+  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
+        if(head == null) return false;
+
         ListNode slow = head;
         ListNode fast = head;
+
         while(fast != null && fast.next != null){
-            slow = slow.next;
             fast = fast.next.next;
-            if(fast == slow) return true;
+            slow = slow.next;
+
+            if(slow == fast){
+                return true;
+            }
         }
+
         return false;
     }
 }
-/**
-TC: O(n)
-SC: O(1)
- */
