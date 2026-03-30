6163608b0cab2311ed84532a99514fb30dc22d7f
Corey <150191535+ZMMMao@users.noreply.github.com>
2026-03-30 18:37:28 -0400

    1 ms (78.18%) 46.6 MB (74.12%) Easy

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array-03-30-2026-18-37-28.java
@@ -0,0 +1,17 @@
+/**
+slow fast two pointers
+TC: O(n)
+SC: O(1)
+ */
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        int count = 1;
+        for(int i = 1; i<nums.length; i++){
+            if(nums[i] != nums[i - 1]){
+                nums[count++] = nums[i];
+            }
+        } 
+        return count;
+    }
+}
