cb156182e517839bc2735319be9e9f1c6fb28a63
Corey <150191535+ZMMMao@users.noreply.github.com>
2026-04-05 17:41:40 -0400

    4 ms (80.57%) 47.1 MB (8.86%) Hard

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists-04-05-2026-17-41-39.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+minHeap, keep k nodes in the heap
+TC: O(klogk)
+SC: O(k)
+*/
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists == null || lists.length == 0) return null;
+
+        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a, b) -> a.val - b.val);
+        for(ListNode list : lists){
+            if(list != null){
+                minHeap.offer(list);
+            }
+        }
+
+        ListNode res = new ListNode();
+        ListNode node = res;
+
+        while(!minHeap.isEmpty()){
+            ListNode curr = minHeap.poll();
+            node.next = curr;
+            node = node.next;
+            if(curr.next != null) minHeap.offer(curr.next);
+        }
+
+        return res.next;
+    }
+}
