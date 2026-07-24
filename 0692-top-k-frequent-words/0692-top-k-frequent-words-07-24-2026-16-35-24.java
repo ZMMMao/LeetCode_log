7eed0d26d423e38758061eb1702a769a330ca17b
Corey <150191535+ZMMMao@users.noreply.github.com>
2026-07-24 16:35:25 -0400

    9 ms (20.83%) 46.5 MB (50.42%) Medium

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words-07-24-2026-16-35-24.java
@@ -0,0 +1,36 @@
+/**
+minheap + hashmap (string, frequency)
+return: list of string, sorted by their lexicographical order
+TC: O(nlogk)
+SC: O(n)
+ */
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        if(words == null || words.length == 0) return new ArrayList<>();
+        
+        Map<String, Integer> freq = new HashMap<>();
+        for(String w : words){
+            freq.put(w, freq.getOrDefault(w, 0) + 1);
+        }
+
+        PriorityQueue<String> minheap = new PriorityQueue<>(
+            (a, b) -> freq.get(a).equals(freq.get(b)) 
+                        ? b.compareTo(a) 
+                        : freq.get(a) - freq.get(b)
+        );
+
+        for(String word : freq.keySet()){
+            minheap.offer(word);
+            if(minheap.size() > k) minheap.poll();
+        }
+
+        List<String> res = new ArrayList<>();
+
+        while(!minheap.isEmpty()){
+            res.addFirst(minheap.poll());
+        }
+
+        return res;
+    }
+
+}
