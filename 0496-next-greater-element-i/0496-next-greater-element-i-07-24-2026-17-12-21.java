f1d8178b7ab80ff04d05300c6e579d65bafa0d1e
Corey <150191535+ZMMMao@users.noreply.github.com>
2026-07-24 17:12:21 -0400

    4 ms (74.06%) 45.4 MB (22.42%) Easy

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i-07-24-2026-17-12-21.java
@@ -0,0 +1,35 @@
+/**
+monostack
+nums1 is the subset of nums2
+distinct elements
+map (nums2) currNum - nextGreaterIdx
+TC: O(n)
+SC: O(n)
+ */
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        if(nums2 == null || nums2.length == 0) return new int[0];
+
+        Map<Integer, Integer> numToIdx = new HashMap<>();
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for(int i = 0; i < nums2.length; i++){
+            while(!stack.isEmpty() && stack.peek() < nums2[i]){
+                int key = stack.pop();
+                numToIdx.put(key, i);
+            }
+            stack.push(nums2[i]);
+        }
+
+        int[] res = new int[nums1.length];
+        for(int j = 0; j<nums1.length; j++){
+            if(numToIdx.get(nums1[j]) == null){
+                res[j] = -1;
+            }else{
+                res[j] = nums2[numToIdx.get(nums1[j])];
+            }
+        }
+
+        return res;
+    }
+}
