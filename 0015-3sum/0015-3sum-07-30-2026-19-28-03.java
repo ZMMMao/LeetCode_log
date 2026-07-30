db0d976319012641b7a0f3e401718764a700ef53
Corey <150191535+ZMMMao@users.noreply.github.com>
2026-07-30 19:28:04 -0400

    30 ms (88.52%) 59.2 MB (60.26%) Medium

---
+++ 0015-3sum/0015-3sum-07-30-2026-19-28-03.java
@@ -0,0 +1,37 @@
+/**
+3 sum, no duplicate triplets
+fixed i with j,k as two pointer to search, hashset as result
+TC: O(n^2)
+SC: O(logn) - by the Arrays.sort(nums)
+ */
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        if(nums == null || nums.length < 3) return new ArrayList<>();
+
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i = 0; i<nums.length-2; i++){
+            if(nums[i] > 0) break;
+            if(i>0 && nums[i] == nums[i-1]) continue;
+
+            int target = -nums[i];
+            int left = i+1;
+            int right = nums.length - 1;
+            while(left < right){
+                int sum = nums[left] + nums[right];
+                if(sum == target){
+                    res.add(List.of(nums[i], nums[left], nums[right]));
+                    while(left < right && nums[left] == nums[left+1])left++;
+                    while(left < right && nums[right] == nums[right-1]) right--;
+                    left++;
+                    right--;
+                }else if(sum > target){
+                    right--;
+                }else if(sum < target){
+                    left++;
+                }
+            }
+        }
+        return res;
+    }
+}
