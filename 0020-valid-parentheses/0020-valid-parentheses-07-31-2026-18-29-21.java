475931de694a960784605f440837acf3a06d7a7e
Corey <150191535+ZMMMao@users.noreply.github.com>
2026-07-31 18:29:21 -0400

    4 ms (36.92%) 43.4 MB (26.1%) Easy

---
+++ 0020-valid-parentheses/0020-valid-parentheses-07-31-2026-18-29-21.java
@@ -0,0 +1,28 @@
+/**
+stack
+peek == curr, pop if not left - push , right return false;
+ */
+class Solution {
+    public boolean isValid(String s) {
+        if(s == null || s.length() <= 1) return false;
+
+        Deque<Character> stack = new ArrayDeque<>();
+        Map<Character, Character> match = new HashMap<>();
+        match.put(']', '[');
+        match.put('}', '{');
+        match.put(')', '(');
+
+        for(int i = 0; i< s.length(); i++){
+            char curr = s.charAt(i);
+            if(curr == '(' || curr == '{' || curr == '['){
+                stack.push(curr);
+            }else if(!stack.isEmpty() && stack.peek() == match.get(curr)){
+                stack.pop();
+            }else{
+                return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
