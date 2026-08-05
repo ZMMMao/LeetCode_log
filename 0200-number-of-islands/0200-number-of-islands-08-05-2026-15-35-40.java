7b23676c5089d90bb18cfbc8018e1f466497ab45
Corey <150191535+ZMMMao@users.noreply.github.com>
2026-08-05 15:35:40 -0400

    5 ms (28.02%) 51.3 MB (92.53%) Medium

---
+++ 0200-number-of-islands/0200-number-of-islands-08-05-2026-15-35-40.java
@@ -0,0 +1,50 @@
+/**
+BFS, level order traversal - queue
+for each 1, bfs traverse and flip 1 to 0
+flooding
+TC: O(n^2)
+SC: O(1)
+ */
+class Solution {
+    public int numIslands(char[][] grid) {
+       if(grid == null || grid.length == 0) return -1;
+        int m = grid.length; //row
+        int n = grid[0].length; // col
+        int res = 0;
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == '1'){
+                    bfs(grid, i, j);
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+
+    private final int[][] DIRC = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+    private void bfs(char[][] grid, int r, int c){
+        Queue<int[]> q = new LinkedList<>();
+
+        q.offer(new int[]{r, c});
+
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i = 0; i < size; i++){    
+                int[] cur = q.poll();
+                grid[cur[0]][cur[1]] = '0';
+                for(int[] d : DIRC){
+                    int x = cur[0] + d[0];
+                    int y = cur[1] + d[1];
+                    if(x < grid.length && x >= 0 && y >= 0 && y < grid[0].length && grid[x][y] == '1'){
+                        q.offer(new int[]{x, y});
+                        grid[x][y] = '0';
+                    }
+                }
+            }
+        }
+        return;
+    }
+}
