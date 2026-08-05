1b2569c9e22b9502323b5dc7aab7b80ec18b542c
Corey <150191535+ZMMMao@users.noreply.github.com>
2026-08-05 16:31:26 -0400

    1 ms (79.76%) 43.6 MB (63.51%) Medium

---
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal-08-05-2026-16-31-25.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/**
+zigzag left to right and next level r to l
+BFS level traverse, with a boolean mark
+TC: O(n)
+SC: O(n)
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        if (root == null)
+            return new ArrayList<>();
+
+        List<List<Integer>> res = new ArrayList<>();
+        bfs(root, res);
+        return res;
+    }
+
+    private void bfs(TreeNode root, List<List<Integer>> res) {
+        Queue<TreeNode> level = new ArrayDeque<>();
+        boolean left = true;
+        level.offer(root);
+        while (!level.isEmpty()) {
+            int size = level.size();
+            List<Integer> l = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = level.poll();
+                l.add(curr.val);
+                if (curr.left != null)
+                    level.offer(curr.left);
+                if (curr.right != null)
+                    level.offer(curr.right);
+            }
+            if (left)
+                res.add(l);
+            else {
+                Collections.reverse(l);
+                res.add(l);
+            }
+            left = !left;
+        }
+    }
+}
