a21f586c9bb85065394601220c049ce9a1afb628
Corey <150191535+ZMMMao@users.noreply.github.com>
2026-08-06 00:27:00 -0400

    6 ms (72.73%) 45.1 MB (68.26%) Medium

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii-08-06-2026-00-27-00.java
@@ -0,0 +1,35 @@
+/**
+DFS
+can't re-use and no duplicate
+helper function
+target == 0, add result
+for each element as a for loop
+select or not, iterate through and target - new added num
+TC: O(n*2^n))
+SC: O(n)
+ */
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        if(candidates == null || candidates.length == 0) return new ArrayList<>();
+
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(candidates);
+        dfs(candidates, 0, target, res, new ArrayList<>());
+        return res;        
+    }
+
+    private void dfs(int[] c, int start, int target, List<List<Integer>> res, List<Integer> path){
+        if(target == 0){
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for(int i = start; i < c.length; i++){
+            if(i > start && c[i] == c[i-1]) continue;
+            if(target < c[i]) break;
+            path.add(c[i]);
+            dfs(c, i+1, target - c[i], res, path);
+            path.remove(path.size() - 1);
+        }
+    }
+}
