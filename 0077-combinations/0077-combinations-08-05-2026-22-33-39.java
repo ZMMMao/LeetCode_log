afeb478b9764eab70e8fff27a7032713f059f5fc
Corey <150191535+ZMMMao@users.noreply.github.com>
2026-08-05 22:33:39 -0400

    18 ms (80.24%) 98 MB (46.92%) Medium

---
+++ 0077-combinations/0077-combinations-08-05-2026-22-33-39.java
@@ -0,0 +1,26 @@
+/**
+dfs combinations
+TC: O(k * C(n, k))
+SC: O(k)
+ */
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        if(n < k) return new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(n, k, 1, res, new ArrayList<>());
+        return res;
+    }
+
+    private void dfs(int n, int k, int start, List<List<Integer>> res, List<Integer> path){
+        if(path.size() == k){
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for(int i = start; i <= n; i++){
+            path.add(i);
+            dfs(n, k, i+1, res, path);
+            path.remove(path.size() - 1);
+        }
+    }
+}
